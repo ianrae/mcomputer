f7e22f208b202c5d1a355f81a72f27baaf97683f
ianrae <windward37@hotmail.com>
2013-10-09 18:24:02 -0400

    copied in Computer

--- project/Build.scala
+++ project/Build.scala
@@ -4,19 +4,18 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "mcomputer"
-  val appVersion      = "1.0-SNAPSHOT"
+    val appName         = "mcomputer"
+    val appVersion      = "1.0"
 
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    javaCore,
-    javaJdbc,
-    javaEbean,
-	"postgresql" % "postgresql" % "8.4-702.jdbc4"	
-  )
+    val appDependencies = Seq(
+      javaCore,
+      javaJdbc,
+      javaEbean
+    )
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+    val main = play.Project(appName, appVersion, appDependencies).settings(
+      // Add your own project settings here      
+    )
 
 }
+            
